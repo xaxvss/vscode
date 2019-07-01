a61070ed46e80f8759f8768536bde6a5a56892fe
Pine Wu <octref@gmail.com>
2019-07-01 10:45:25 -0700

    Clean up emmet deps

--- extensions/emmet/src/test/index.ts
+++ extensions/emmet/src/test/index.ts
@@ -3,28 +3,53 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
-const path = require('path');
-const testRunner = require('vscode/lib/testrunner');
+import * as path from 'path';
+import * as Mocha from 'mocha';
+import * as glob from 'glob';
 
 const suite = 'Integration Emmet Tests';
 
-const options: any = {
-	ui: 'tdd',
-	useColors: true,
-	timeout: 60000
-};
+export function run(): Promise<void> {
+	// Create the mocha test
+	const mocha = new Mocha({
+		ui: 'tdd',
+		timeout: 60000
+	});
+	mocha.useColors(true);
 
-if (process.env.BUILD_ARTIFACTSTAGINGDIRECTORY) {
-	options.reporter = 'mocha-multi-reporters';
-	options.reporterOptions = {
-		reporterEnabled: 'spec, mocha-junit-reporter',
-		mochaJunitReporterReporterOptions: {
-			testsuitesTitle: `${suite} ${process.platform}`,
-			mochaFile: path.join(process.env.BUILD_ARTIFACTSTAGINGDIRECTORY, `test-results/${process.platform}-${suite.toLowerCase().replace(/[^\w]/g, '-')}-results.xml`)
-		}
-	};
+	if (process.env.BUILD_ARTIFACTSTAGINGDIRECTORY) {
+		mocha.reporter('mocha-multi-reporters', {
+			reporterEnabled: 'spec, mocha-junit-reporter',
+			mochaJunitReporterReporterOptions: {
+				testsuitesTitle: `${suite} ${process.platform}`,
+				mochaFile: path.join(process.env.BUILD_ARTIFACTSTAGINGDIRECTORY, `test-results/${process.platform}-${suite.toLowerCase().replace(/[^\w]/g, '-')}-results.xml`)
+			}
+		});
+	}
+
+	const testsRoot = path.resolve(__dirname, '..');
+
+	return new Promise((c, e) => {
+		glob('**/**.test.js', { cwd: testsRoot }, (err, files) => {
+			if (err) {
+				return e(err);
+			}
+
+			// Add files to the test suite
+			files.forEach(f => mocha.addFile(path.resolve(testsRoot, f)));
+
+			try {
+				// Run the mocha test
+				mocha.run(failures => {
+					if (failures > 0) {
+						e(new Error(`${failures} tests failed.`));
+					} else {
+						c();
+					}
+				});
+			} catch (err) {
+				e(err);
+			}
+		});
+	});
 }
-
-testRunner.configure(options);
-
-export = testRunner;
